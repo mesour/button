ae9d440d18c06343570ec52378f24f789d82ddd3
mesour <matous.nemec@gmail.com>
2016-03-09 12:58:35 +0100

    Reformat code, moved text translate to render

--- src/Mesour/UI/Button.php
+++ src/Mesour/UI/Button.php
@@ -19,271 +19,279 @@ use Mesour;
 class Button extends Mesour\Components\Control\AttributesControl
 {
 
-    const WRAPPER = 'wrapper',
-        DEFAULTS = 'defaults';
+	const WRAPPER = 'wrapper';
+	const DEFAULTS = 'defaults';
 
-    private $text = '';
+	private $text = '';
 
-    private $disabled = FALSE;
+	private $html = null;
 
-    private $className = NULL;
+	private $disabled = false;
 
-    public $onRender = [];
+	private $className = null;
 
-    protected $defaults = [
-        self::WRAPPER => [
-            'el' => 'a',
-            'attributes' => [
-                'class' => 'btn btn-{_BTN_type_} {_BTN_size_}',
-                'role' => 'button',
-            ]
-        ],
-        self::DEFAULTS => [
-            '_BTN_type_' => 'default',
-            '_BTN_size_' => '',
-            '_ICON_name_' => 'cog',
-            '_ICON_prefix_' => 'fa fa-',
-            '_RICON_name_' => 'cog',
-            '_RICON_prefix_' => 'fa fa-',
-        ]
-    ];
+	public $onRender = [];
 
-    public function __construct($name = NULL, Mesour\Components\ComponentModel\IContainer $parent = NULL)
-    {
-        parent::__construct($name, $parent);
+	protected $defaults = [
+		self::WRAPPER => [
+			'el' => 'a',
+			'attributes' => [
+				'class' => 'btn btn-{_BTN_type_} {_BTN_size_}',
+				'role' => 'button',
+			],
+		],
+		self::DEFAULTS => [
+			'_BTN_type_' => 'default',
+			'_BTN_size_' => '',
+			'_ICON_name_' => 'cog',
+			'_ICON_prefix_' => 'fa fa-',
+			'_RICON_name_' => 'cog',
+			'_RICON_prefix_' => 'fa fa-',
+		],
+	];
 
-        $this->setHtmlElement(
-            Mesour\Components\Utils\Html::el($this->getOption(self::WRAPPER, 'el'))
-        );
-    }
+	public function __construct($name = null, Mesour\Components\ComponentModel\IContainer $parent = null)
+	{
+		parent::__construct($name, $parent);
 
-    /**
-     * @return Mesour\Components\Utils\Html
-     */
-    public function getControlPrototype()
-    {
-        return $this->getHtmlElement();
-    }
+		$this->setHtmlElement(
+			Mesour\Components\Utils\Html::el($this->getOption(self::WRAPPER, 'el'))
+		);
+	}
 
-    public function setConfirm($message)
-    {
-        $this->setAttribute('onclick', 'return confirm(\'' . str_replace("'", "\\'", $this->getTranslator()->translate($message)) . '\');');
-        return $this;
-    }
+	/**
+	 * @return Mesour\Components\Utils\Html
+	 */
+	public function getControlPrototype()
+	{
+		return $this->getHtmlElement();
+	}
 
-    /**
-     * @param $title
-     * @param string $placement left|top|bottom|right
-     * @return $this
-     */
-    public function setTooltip($title, $placement = 'top')
-    {
-        $this->setAttribute('title', $title);
-        $this->setAttribute('data-mesour-toggle', 'tooltip');
-        $this->setAttribute('data-placement', $placement);
-        return $this;
-    }
+	public function setConfirm($message)
+	{
+		$this->setAttribute('onclick', 'return confirm(\'' . str_replace("'", "\\'", $this->getTranslator()->translate($message)) . '\');');
+		return $this;
+	}
 
-    public function setText($text)
-    {
-        return $this->setHtml(htmlspecialchars($this->getTranslator()->translate($text)));
-    }
+	/**
+	 * @param $title
+	 * @param string $placement left|top|bottom|right
+	 * @return $this
+	 */
+	public function setTooltip($title, $placement = 'top')
+	{
+		$this->setAttribute('title', $title);
+		$this->setAttribute('data-mesour-toggle', 'tooltip');
+		$this->setAttribute('data-placement', $placement);
+		return $this;
+	}
 
-    public function setHtml($html)
-    {
-        $this->text = $html;
-        return $this;
-    }
+	public function setText($text)
+	{
+		$this->text = htmlspecialchars($text);
+		return $this;
+	}
 
-    public function setSize($size_class)
-    {
-        $this->setOption(self::DEFAULTS, $size_class, '_BTN_size_');
-        return $this;
-    }
+	public function setHtml($html)
+	{
+		$this->html = $html;
+		return $this;
+	}
 
-    public function setType($type)
-    {
-        $this->setOption(self::DEFAULTS, $type, '_BTN_type_');
-        return $this;
-    }
+	public function setSize($size_class)
+	{
+		$this->setOption(self::DEFAULTS, $size_class, '_BTN_size_');
+		return $this;
+	}
 
-    public function setDisabled($disabled = TRUE)
-    {
-        $this->disabled = (bool)$disabled;
-        return $this;
-    }
+	public function setType($type)
+	{
+		$this->setOption(self::DEFAULTS, $type, '_BTN_type_');
+		return $this;
+	}
 
-    public function setClassName($className)
-    {
-        $this->className = $className;
-        return $this;
-    }
+	public function setDisabled($disabled = true)
+	{
+		$this->disabled = (bool)$disabled;
+		return $this;
+	}
 
-    public function isDisabled()
-    {
-        return $this->disabled;
-    }
+	public function setClassName($className)
+	{
+		$this->className = $className;
+		return $this;
+	}
 
-    public function setIcon($type = NULL, $classPrefix = NULL)
-    {
-        if ($this->hasLeftIcon()) {
-            $icon = $this->getLeftIcon()
-                ->setType(!$type ? $this->getOption(self::DEFAULTS, '_ICON_name_') : $type)
-                ->setPrefix(!$classPrefix ? $this->getOption(self::DEFAULTS, '_ICON_prefix_') : $classPrefix);
-            return $icon;
-        }
-        $this->createIcon(
-            'leftIcon',
-            !$type ? $this->getOption(self::DEFAULTS, '_ICON_name_') : $type,
-            !$classPrefix ? $this->getOption(self::DEFAULTS, '_ICON_prefix_') : $classPrefix
-        );
-        return $this;
-    }
+	public function isDisabled()
+	{
+		return $this->disabled;
+	}
 
-    public function setRightIcon($type = NULL, $classPrefix = NULL)
-    {
-        if ($this->hasRightIcon()) {
-            $icon = $this->getRightIcon()
-                ->setType(!$type ? $this->getOption(self::DEFAULTS, '_RICON_name_') : $type)
-                ->setPrefix(!$classPrefix ? $this->getOption(self::DEFAULTS, '_RICON_prefix_') : $classPrefix);
-            return $icon;
-        }
-        $this->createIcon(
-            'rightIcon',
-            !$type ? $this->getOption(self::DEFAULTS, '_RICON_name_') : $type,
-            !$classPrefix ? $this->getOption(self::DEFAULTS, '_RICON_prefix_') : $classPrefix
-        );
-        return $this;
-    }
+	public function setIcon($type = null, $classPrefix = null)
+	{
+		if ($this->hasLeftIcon()) {
+			$icon = $this->getLeftIcon()
+				->setType(!$type ? $this->getOption(self::DEFAULTS, '_ICON_name_') : $type)
+				->setPrefix(!$classPrefix ? $this->getOption(self::DEFAULTS, '_ICON_prefix_') : $classPrefix);
+			return $icon;
+		}
+		$this->createIcon(
+			'leftIcon',
+			!$type ? $this->getOption(self::DEFAULTS, '_ICON_name_') : $type,
+			!$classPrefix ? $this->getOption(self::DEFAULTS, '_ICON_prefix_') : $classPrefix
+		);
+		return $this;
+	}
 
-    public function setPermission($resource = NULL, $privilege = NULL)
-    {
-        $this->setPermissionCheck($resource, $privilege);
-        return $this;
-    }
+	public function setRightIcon($type = null, $classPrefix = null)
+	{
+		if ($this->hasRightIcon()) {
+			$icon = $this->getRightIcon()
+				->setType(!$type ? $this->getOption(self::DEFAULTS, '_RICON_name_') : $type)
+				->setPrefix(!$classPrefix ? $this->getOption(self::DEFAULTS, '_RICON_prefix_') : $classPrefix);
+			return $icon;
+		}
+		$this->createIcon(
+			'rightIcon',
+			!$type ? $this->getOption(self::DEFAULTS, '_RICON_name_') : $type,
+			!$classPrefix ? $this->getOption(self::DEFAULTS, '_RICON_prefix_') : $classPrefix
+		);
+		return $this;
+	}
 
-    public function create()
-    {
-        parent::create();
+	public function setPermission($resource = null, $privilege = null)
+	{
+		$this->setPermissionCheck($resource, $privilege);
+		return $this;
+	}
 
-        $wrapper = $this->getControlPrototype();
-        $oldWrapper = clone $wrapper;
-        foreach ($oldWrapper->attrs as $key => $attr) {
-            if (is_object($attr)) {
-                $oldWrapper->attrs[$key] = clone $attr;
-            }
-        }
+	public function create()
+	{
+		parent::create();
 
-        if ($this->hasLeftIcon()) {
-            $oldLeftIcon = clone $this->getLeftIcon();
-        }
-        if ($this->hasRightIcon()) {
-            $oldRightIcon = clone $this->getRightIcon();
-        }
+		$wrapper = $this->getControlPrototype();
+		$oldWrapper = clone $wrapper;
+		foreach ($oldWrapper->attrs as $key => $attr) {
+			if (is_object($attr)) {
+				$oldWrapper->attrs[$key] = clone $attr;
+			}
+		}
 
-        $this->onRender($this);
+		if ($this->hasLeftIcon()) {
+			$oldLeftIcon = clone $this->getLeftIcon();
+		}
+		if ($this->hasRightIcon()) {
+			$oldRightIcon = clone $this->getRightIcon();
+		}
 
-        $optionData = $this->getOption(self::DEFAULTS);
+		$this->onRender($this);
 
-        if (!$this->isAllowed()) {
-            $this->setDisabled(TRUE);
-        }
+		$optionData = $this->getOption(self::DEFAULTS);
 
-        foreach ($this->getOption(self::WRAPPER, 'attributes') as $key => $value) {
-            if ($value instanceof Mesour\Components\Link\IUrl) {
-                continue;
-            }
-            if (!$this->hasAttribute($key) || $this->getAttribute($key, FALSE) !== FALSE) {
-                $value = trim(Mesour\Components\Utils\Helpers::parseValue($value, $optionData));
-                if ($this->hasAttribute($key)) {
-                    $value .= ' ' . $this->getAttribute($key);
-                }
-                $this->setAttribute($key, $value);
-            }
-        }
+		if (!$this->isAllowed()) {
+			$this->setDisabled(true);
+		}
 
-        if (!is_null($this->className)) {
-            $this->setAttribute('class', $this->className === '' ? FALSE : $this->className);
-        }
+		foreach ($this->getOption(self::WRAPPER, 'attributes') as $key => $value) {
+			if ($value instanceof Mesour\Components\Link\IUrl) {
+				continue;
+			}
+			if (!$this->hasAttribute($key) || $this->getAttribute($key, false) !== false) {
+				$value = trim(Mesour\Components\Utils\Helpers::parseValue($value, $optionData));
+				if ($this->hasAttribute($key)) {
+					$value .= ' ' . $this->getAttribute($key);
+				}
+				$this->setAttribute($key, $value);
+			}
+		}
 
-        $this->getAttributes($this->isDisabled());
+		if (!is_null($this->className)) {
+			$this->setAttribute('class', $this->className === '' ? false : $this->className);
+		}
 
-        if ($this->hasLeftIcon()) {
-            $leftIcon = $this->getLeftIcon();
-            $leftIcon->setOption('data', $this->getOption('data'));
-            $wrapper->add($leftIcon->create());
-            if (strlen($this->text) > 0) {
-                $wrapper->add('&nbsp;');
-            }
-            if (isset($oldLeftIcon)) {
-                unset($this['leftIcon']);
-                $this['leftIcon'] = $oldLeftIcon;
-            }
-        }
+		$this->getAttributes($this->isDisabled());
 
-        $wrapper->add($this->text);
+		if ($this->hasLeftIcon()) {
+			$leftIcon = $this->getLeftIcon();
+			$leftIcon->setOption('data', $this->getOption('data'));
+			$wrapper->add($leftIcon->create());
+			if (strlen($this->text) > 0) {
+				$wrapper->add('&nbsp;');
+			}
+			if (isset($oldLeftIcon)) {
+				unset($this['leftIcon']);
+				$this['leftIcon'] = $oldLeftIcon;
+			}
+		}
 
-        if ($this->hasRightIcon()) {
-            $rightIcon = $this->getRightIcon();
-            $rightIcon->setOption('data', $this->getOption('data'));
-            if (strlen($this->text) > 0) {
-                $wrapper->add('&nbsp;');
-            }
-            $wrapper->add($rightIcon->create());
-            if (isset($oldRightIcon)) {
-                unset($this['rightIcon']);
-                $this['rightIcon'] = $oldRightIcon;
-            }
-        }
-        $this->setHtmlElement($oldWrapper);
-        return $wrapper;
-    }
+		if ($this->text) {
+			$wrapper->add($this->getTranslator()->translate($this->text));
+		}
+		if ($this->html) {
+			$wrapper->add($this->html);
+		}
 
-    /**
-     * @return Icon
-     * @throws Mesour\InvalidStateException
-     */
-    public function getLeftIcon()
-    {
-        if (!$this->hasLeftIcon()) {
-            throw new Mesour\InvalidStateException('Left icon is not created.');
-        }
-        return $this['leftIcon'];
-    }
+		if ($this->hasRightIcon()) {
+			$rightIcon = $this->getRightIcon();
+			$rightIcon->setOption('data', $this->getOption('data'));
+			if (strlen($this->text) > 0) {
+				$wrapper->add('&nbsp;');
+			}
+			$wrapper->add($rightIcon->create());
+			if (isset($oldRightIcon)) {
+				unset($this['rightIcon']);
+				$this['rightIcon'] = $oldRightIcon;
+			}
+		}
+		$this->setHtmlElement($oldWrapper);
+		return $wrapper;
+	}
 
-    public function hasLeftIcon()
-    {
-        return isset($this['leftIcon']);
-    }
+	/**
+	 * @return Icon
+	 * @throws Mesour\InvalidStateException
+	 */
+	public function getLeftIcon()
+	{
+		if (!$this->hasLeftIcon()) {
+			throw new Mesour\InvalidStateException('Left icon is not created.');
+		}
+		return $this['leftIcon'];
+	}
 
-    /**
-     * @return Icon
-     * @throws Mesour\InvalidStateException
-     */
-    public function getRightIcon()
-    {
-        if (!$this->hasRightIcon()) {
-            throw new Mesour\InvalidStateException('Right icon is not created.');
-        }
-        return $this['rightIcon'];
-    }
+	public function hasLeftIcon()
+	{
+		return isset($this['leftIcon']);
+	}
 
-    public function hasRightIcon()
-    {
-        return isset($this['rightIcon']);
-    }
+	/**
+	 * @return Icon
+	 * @throws Mesour\InvalidStateException
+	 */
+	public function getRightIcon()
+	{
+		if (!$this->hasRightIcon()) {
+			throw new Mesour\InvalidStateException('Right icon is not created.');
+		}
+		return $this['rightIcon'];
+	}
 
-    protected function createIcon($name, $type, $prefix)
-    {
-        $className = $this->getIconClass();
+	public function hasRightIcon()
+	{
+		return isset($this['rightIcon']);
+	}
 
-        /** @var Mesour\Icon\IIcon $icon */
-        $icon = new $className();
+	protected function createIcon($name, $type, $prefix)
+	{
+		$className = $this->getIconClass();
 
-        $icon->setType($type);
-        $icon->setPrefix($prefix);
+		/** @var Mesour\Icon\IIcon $icon */
+		$icon = new $className();
 
-        return $this[$name] = $icon;
-    }
+		$icon->setType($type);
+		$icon->setPrefix($prefix);
+
+		return $this[$name] = $icon;
+	}
 
 }
